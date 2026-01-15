f8206fe260c8d4873bba65d4ca0a78801142906d
Jakov Knezovic <jakov@netgen.io>
2026-01-15 14:06:03 +0100

    NGSTACK-1017 add compiler pass to bundle build and autoconfigure open api processor tag

--- src/NetgenApiPlatformExtrasBundle.php
+++ src/NetgenApiPlatformExtrasBundle.php
@@ -4,16 +4,25 @@ declare(strict_types=1);
 
 namespace Netgen\ApiPlatformExtras;
 
-use Netgen\ApiPlatformExtras\DependencyInjection\CompilerPass\IriTemplateGeneratorCompilerPass;
+use Netgen\ApiPlatformExtras\DependencyInjection\CompilerPass\SchemaProcessorCompilerPass;
+use Netgen\ApiPlatformExtras\OpenApi\Processor\OpenApiProcessorInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Netgen\ApiPlatformExtras\DependencyInjection\CompilerPass\IriTemplateGeneratorCompilerPass;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class NetgenApiPlatformExtrasBundle extends Bundle
 {
     public function build(ContainerBuilder $container): void
     {
-        $container->addCompilerPass(
+        $container
+        ->addCompilerPass(
             new IriTemplateGeneratorCompilerPass(),
+        )
+        ->addCompilerPass(
+            new SchemaProcessorCompilerPass(),
         );
+
+        $container->registerForAutoconfiguration(OpenApiProcessorInterface::class)
+            ->addTag('netgen_api_platform_extras.open_api_processor');
     }
 }
