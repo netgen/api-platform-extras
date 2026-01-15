93cb7bc296769b16369b4ad3881bf6aa327ddd17
Jakov Knezovic <jakov@netgen.io>
2026-01-15 14:05:23 +0100

    NGSTACK-1017 add compiler pass for schema processor feature

---
+++ src/DependencyInjection/CompilerPass/SchemaProcessorCompilerPass.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\ApiPlatformExtras\DependencyInjection\CompilerPass;
+
+use Netgen\ApiPlatformExtras\OpenApi\Factory\OpenApiFactory;
+use Symfony\Component\DependencyInjection\Argument\TaggedIteratorArgument;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
+
+final class SchemaProcessorCompilerPass implements CompilerPassInterface
+{
+    private const FEATURE_ENABLED_PARAMETER = 'netgen_api_platform_extras.features.schema_processor.enabled';
+
+    public function process(ContainerBuilder $container): void
+    {
+        if (
+            !$container->hasParameter(self::FEATURE_ENABLED_PARAMETER)
+            || $container->getParameter(self::FEATURE_ENABLED_PARAMETER) === false
+        ) {
+            return;
+        }
+
+        $container
+            ->setDefinition(
+                OpenApiFactory::class,
+                new Definition(OpenApiFactory::class),
+            )
+            ->setArguments([
+                new Reference('api_platform.openapi.factory.inner'),
+                new TaggedIteratorArgument(
+                    tag: 'netgen_api_platform_extras.open_api_processor',
+                    defaultPriorityMethod: 'getPriority',
+                ),
+            ])
+            ->setDecoratedService('api_platform.openapi.factory', 'api_platform.openapi.factory.inner', -25);
+    }
+}
