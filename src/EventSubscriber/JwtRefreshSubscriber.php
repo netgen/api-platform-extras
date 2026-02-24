e800bb61c2fbb5060dc4398313d64593583b68bd
Jakov Knezovic <jakov@netgen.io>
2026-02-24 16:40:01 +0100

    NGSTACK-1017 add kernel.request subscriber for handling refresh

---
+++ src/EventSubscriber/JwtRefreshSubscriber.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\ApiPlatformExtras\EventSubscriber;
+
+use Lexik\Bundle\JWTAuthenticationBundle\TokenExtractor\TokenExtractorInterface;
+use Netgen\ApiPlatformExtras\JwtRefresh\ValueObject\RefreshToken;
+use Netgen\ApiPlatformExtras\Service\RequestTokenResolver;
+use Netgen\ApiPlatformExtras\Service\TokenRefreshService;
+use Symfony\Bundle\SecurityBundle\Security\FirewallConfig;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\Security\Http\FirewallMapInterface;
+
+use function array_find;
+use function in_array;
+use function is_string;
+use function method_exists;
+use function str_starts_with;
+
+final class JwtRefreshSubscriber implements EventSubscriberInterface
+{
+    public function __construct(
+        private TokenExtractorInterface $jwtTokenExtractor,
+        private RequestTokenResolver $tokensResolver,
+        private TokenRefreshService $tokenRefreshService,
+        private FirewallMapInterface $firewallMap,
+        private array $allowedFirewalls,
+        private array $ignoredRoutes,
+        private array $ignoredPaths,
+        private bool $cookieAutoRefreshEnabled,
+        private bool $headerAutoRefreshEnabled,
+    ) {}
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            RequestEvent::class => ['onKernelRequest', 1024],
+        ];
+    }
+
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        if ($this->cookieAutoRefreshEnabled === false && $this->headerAutoRefreshEnabled === false) {
+            return;
+        }
+
+        $request = $event->getRequest();
+        $jwtToken = $this->jwtTokenExtractor->extract($request);
+        if ($jwtToken !== false) {
+            return;
+        }
+
+        $refreshToken = $this->tokensResolver->resolveRefreshTokens($request);
+        if (!$refreshToken instanceof RefreshToken) {
+            return;
+        }
+
+        if (method_exists($this->firewallMap, 'getFirewallConfig')) {
+            $firewallConfig = $this->firewallMap->getFirewallConfig($request);
+
+            if (!$firewallConfig instanceof FirewallConfig) {
+                return;
+            }
+
+            if ($this->allowedFirewalls !== [] && !in_array($firewallConfig->getName(), $this->allowedFirewalls, true)) {
+                return;
+            }
+        } else {
+            return;
+        }
+
+        if (array_find(
+            $this->ignoredPaths,
+            static fn ($path) => is_string($path) && str_starts_with($request->getPathInfo(), $path),
+        ) !== null) {
+            return;
+        }
+
+        if (array_find(
+            $this->ignoredRoutes,
+            static fn ($route) => is_string($route) && $request->attributes->get('_route') === $route,
+        ) !== null) {
+            return;
+        }
+
+        $this->tokenRefreshService->refresh(
+            $request,
+            $refreshToken,
+            $this->headerAutoRefreshEnabled,
+            $this->cookieAutoRefreshEnabled,
+            $firewallConfig->getProvider(),
+        );
+    }
+}
